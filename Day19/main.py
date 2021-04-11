b8da379a50a6ca8216d55b84c4aa42e884206ca2
jessemarder <jessemarder@gmail.com>
2021-04-10 21:39:33 -0400

    Testing out commit via Pycharm for the first time. Adding functionality for space bar and mouse bar.

--- Day19/main.py
+++ Day19/main.py
@@ -1,7 +1,15 @@
 """
 Based on "100 days of code" Day 19 lecture, which is to create an Etch a Sketch
 """
-print("hello")
+
 from turtle import Turtle, Screen
 
+tim = Turtle()
+screen = Screen()
 
+def move_forward():
+    tim.forward(10)
+
+screen.listen()
+screen.onkey(key="space", fun=move_forward)
+screen.exitonclick()
