c162dd0750f63a6e4c7c001a910884e7bf8bbd97
jessemarder <jessemarder@gmail.com>
2021-04-10 21:52:18 -0400

    Finished initial assignment. Pointer can now draw and rotate, screen can be reset and pointer returned to center.

--- Day19/main.py
+++ Day19/main.py
@@ -10,6 +10,21 @@ screen = Screen()
 def move_forward():
     tim.forward(10)
 
+def move_backward():
+    tim.backward(10)
+
+def move_clockwise():
+    tim.right(10)
+def move_counter_clockwise():
+    tim.left(10)
+def reset():
+    tim.home()
+    screen.reset()
+
 screen.listen()
-screen.onkey(key="space", fun=move_forward)
+screen.onkey(key="w", fun=move_forward)
+screen.onkey(key="s", fun=move_backward)
+screen.onkey(key="d", fun=move_clockwise)
+screen.onkey(key="a", fun=move_counter_clockwise)
+screen.onkey(key="c", fun=reset)
 screen.exitonclick()
